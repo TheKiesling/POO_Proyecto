fe00e68f8fe788686410bd2c92cef1c5adeb13ba
Montoya086 <mon21552@uvg.edu.gt>
2021-09-12 20:39:10 -0600

    Corrección de errores en los forms

--- Proyecto_POO/Proyecto_POO/FormIngresoUnidadIGSS.Designer.cs
+++ Proyecto_POO/Proyecto_POO/FormIngresoUnidadIGSS.Designer.cs
@@ -29,12 +29,70 @@ namespace Proyecto_POO
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(255, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese la unidad del IGSS";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(258, 183);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(288, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(258, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Traslado de paciente";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(405, 221);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 43);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // FormIngresoUnidadIGSS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "FormIngresoUnidadIGSS";
             this.Text = "FormIngresoUnidadIGSS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- Proyecto_POO/Proyecto_POO/FormIngresoUnidadIGSS.cs
+++ Proyecto_POO/Proyecto_POO/FormIngresoUnidadIGSS.cs
@@ -16,5 +16,20 @@ namespace Proyecto_POO
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
