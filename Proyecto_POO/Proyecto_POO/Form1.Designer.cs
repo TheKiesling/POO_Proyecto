4754f484d6b226a71dd27a4994c35c34e34fa651
TheKiesling <kie21581@uvg.edu.gt>
2021-09-12 18:06:50 -0600

    Diseño de Menú Principal
    Colocar botones y labels del menú principal con sus respectivos colores

--- Proyecto_POO/Proyecto_POO/Form1.Designer.cs
+++ Proyecto_POO/Proyecto_POO/Form1.Designer.cs
@@ -29,13 +29,123 @@ namespace Proyecto_POO
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(294, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ingresar Paciente";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(294, 135);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(192, 51);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Dar de alta al Paciente";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(294, 192);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(192, 51);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Modificar datos del Paciente";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.DarkCyan;
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.Location = new System.Drawing.Point(294, 249);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(192, 51);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Buscar Paciente";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button5.Location = new System.Drawing.Point(294, 306);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(192, 51);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Traslado de Paciente";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button6.Location = new System.Drawing.Point(294, 363);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(192, 51);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Salir";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(70, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(511, 39);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Seleccione la opción a realizar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Proyecto_POO/Proyecto_POO/Form1.cs
+++ Proyecto_POO/Proyecto_POO/Form1.cs
@@ -16,5 +16,15 @@ namespace Proyecto_POO
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
