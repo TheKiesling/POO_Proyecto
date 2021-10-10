c883fb3178c342dab686f902e8aa65126dbaab5a
Deloz09 <84214640+Deloz09@users.noreply.github.com>
2021-10-10 15:39:23 -0600

    Documentación Clase Usuario Admin

--- Proyecto_POO/Proyecto_POO/Resources/Usuario_admin.cs
+++ Proyecto_POO/Proyecto_POO/Resources/Usuario_admin.cs
@@ -1,14 +1,30 @@
 ﻿using System;
 
 public class Usuario_admin : Usuario {
+    /**
+    @author: Diego E. Lemus L.
+    Nombre del programa: Usuario_admin.cs
+    @version: 
+        - Creación: 10/10/2021
+        - Última modificación: 10/10/2021
 
+    Clase hija de Usuario que tiene los métodos para el manejo de usuarios en general
+     */
+
+    //---------------------------PROPIEDADES-------------------------
     private Hospital hospital;
-    
+
+    //-----------------------------MÉTODOS---------------------------
     public Usuario_admin(String codigo, String contrasena, String tipo)
         : base(codigo, contrasena, tipo)
     {
        
     }
+
+    /****
+     * Modificar Paciente
+     * @param numero_afiliacion
+     */
     public override bool modificarPaciente(String numeroDeAfiliacion) 
     {
         bool bandera = false;
@@ -16,6 +32,11 @@ public class Usuario_admin : Usuario {
 
         return bandera;
     }
+
+    /****
+     * Dar de alta al Paciente
+     * @param numero_afiliacion
+     */
     public override bool darAltaPaciente(String numeroDeAfiliacion)
     {
         bool bandera = false;
@@ -24,6 +45,10 @@ public class Usuario_admin : Usuario {
         return bandera;
     }
 
+    /****
+     * Traslado Paciente
+     * @param numero_afiliacion
+     */
     public override bool trasladoPaciente(String numeroDeAfiliacion)
     {
         bool bandera = false;
@@ -32,6 +57,10 @@ public class Usuario_admin : Usuario {
         return bandera;
     }
 
+    /****
+     * Ingresar Paciente
+     * @param null
+     */
     public override bool ingresarPaciente()
     {
         return false;
