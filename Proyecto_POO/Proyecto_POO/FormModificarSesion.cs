a098dcca1f94b78ed7ae993dfa5b267b5e624108
Deloz09 <84214640+Deloz09@users.noreply.github.com>
2021-10-17 23:58:23 -0600

    Funcionamiento de Modificar Sesión

--- Proyecto_POO/Proyecto_POO/FormModificarSesion.cs
+++ Proyecto_POO/Proyecto_POO/FormModificarSesion.cs
@@ -19,14 +19,61 @@ namespace Proyecto_POO
             this.hospital = hospital;
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        string nombre, contraseña;
+        public string obtenernombre()
         {
-            FormInicioSesion IS = new FormInicioSesion(hospital);
-            IS.Show();//mostrar el form del menú
-            this.Close();//cerrar este form
+            return nombre;
+        }
+
+        private void inicioExitoso(String s)
+        {
+            MessageBox.Show(s);
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+            string usuario = textBox1.Text;
+            string contraseña = textBox2.Text;
+            string usuario2 = textBox4.Text;
+            string contraseña2 = textBox3.Text;
+
+            if (usuario == "" || contraseña == "" ||  usuario2 == "" || contraseña2 == "")
+            {
+                inicioExitoso("Por favor llenar todos los datos");
+            }
+            else if (usuario == "" || contraseña == "")
+            {
+                inicioExitoso("Por favor llenar todos los datos de su sesion anterior");
+            }
+            else if (usuario2 == "" || contraseña2 == "")
+            {
+                inicioExitoso("Por favor llenar todos los datos de su sesion nueva");
+            }
+            else
+            {
+                Usuario user = null;
+                if (contraseña == "123POO" && usuario == "ADMIN")
+                {
+                    user = new Usuario_admin(usuario2, contraseña2, "admin");
+                    FormMain m = new FormMain(hospital, user);
+                    m.Show();//mostrar el form del menú
+                    this.Close();//cerrar este form
+                }
+                else if (contraseña != "123POO" && usuario == "ADMIN")
+                {
+                    inicioExitoso("Datos de sesion no registrados");
+                }
+                else
+                {
+                    user = new Usuario_paciente(usuario2, contraseña2, "paciente");
+                    FormMain m = new FormMain(hospital, user);
+                    m.Show();//mostrar el form del menú
+                    this.Close();//cerrar este form
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
         {
             FormInicioSesion IS = new FormInicioSesion(hospital);
             IS.Show();//mostrar el form del menú
