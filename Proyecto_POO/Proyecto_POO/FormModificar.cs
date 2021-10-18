8a68a17455b3826194746980bd2411eb1d3a43fb
DC1823 <cue21965@uvg.edu.gt>
2021-10-17 23:15:18 -0600

    Validacion modificar paciente

--- Proyecto_POO/Proyecto_POO/FormModificar.cs
+++ Proyecto_POO/Proyecto_POO/FormModificar.cs
@@ -56,6 +56,29 @@ namespace Proyecto_POO
         private void button6_Click(object sender, EventArgs e)
         {
 
+            if (textBox8.Text == "")
+            {
+                string[] datos = hospital.buscar_paciente(textBox8.Text);
+                textBox1.Text = datos[0];
+                textBox2.Text = datos[1];
+                textBox3.Text = datos[2];
+                textBox4.Text = datos[3];
+                textBox5.Text = datos[4];
+                textBox7.Text = datos[5];
+                textBox6.Text = datos[6];
+                if (datos.Contains<string>("") || textBox1.Text == "")
+                {
+                    MessageBox.Show("No se ha podido encontrar");
+                }
+                else
+                {
+                    MessageBox.Show("Se ha podido encontrar al paciente correctamente");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor ingrese un numero de afiliacion.");
+            }
         }
     }
 }
