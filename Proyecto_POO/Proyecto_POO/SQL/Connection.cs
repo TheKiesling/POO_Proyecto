b9382681d883083c06551bcf51c988ca95e82950
TheKiesling <kie21581@uvg.edu.gt>
2021-10-23 15:26:25 -0600

    Propiedades clase conexion

---
+++ Proyecto_POO/Proyecto_POO/SQL/Connection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Proyecto_POO.SQL
+{
+    class Connection
+    {
+        MySqlConnection connection = new MySqlConnection();
+
+        private static string server = "localhost";
+        private static string database = "sgh";
+        private static string user = "root";
+        private static string password = "";
+
+        private string connect = "server=" + server + ";" + "user=" + user + ";" + "password=" + password + ";" + "database=" + database + ";";
+    }
+}
